5c313cda3ceba671dc181acae5604326694924e4
eiselems <marcus.eisele@gmail.com>
2016-05-12 12:17:10 +0200

    add script for rewriting git authors

---
+++ rewriteGitAuthor.sh
@@ -0,0 +1,18 @@
+# rewrites every commit done by WRONG_EMAIL to NEW_NAME / NEW_EMAIL
+# BE AWARE: YOU ARE REWRITTING THOSE COMMITS 
+git filter-branch --env-filter '
+WRONG_EMAIL="wrong@example.com"
+NEW_NAME="New Name Value"
+NEW_EMAIL="correct@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
